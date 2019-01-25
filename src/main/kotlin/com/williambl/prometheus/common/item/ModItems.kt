85c6ff8800a338d039986a733da3fdaf85ce0bae
williambl <william@williambl.com>
2019-01-25 23:34:11 +0000

    Add Multiblock Starter Item

--- src/main/kotlin/com/williambl/prometheus/common/item/ModItems.kt
+++ src/main/kotlin/com/williambl/prometheus/common/item/ModItems.kt
@@ -1,5 +1,7 @@
 package com.williambl.prometheus.common.item
 
+import com.williambl.prometheus.common.item.base.BaseItem
+import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod
@@ -8,7 +10,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 @Mod.EventBusSubscriber
 object ModItems {
 
-    val items = listOf<Item>()
+    val multiBlockStarter = BaseItem("multiblockstarter", CreativeTabs.REDSTONE)
+
+    val items = listOf<Item>(multiBlockStarter)
 
     @SubscribeEvent
     @JvmStatic
--- src/main/kotlin/com/williambl/prometheus/common/item/base/BaseItem.kt
+++ src/main/kotlin/com/williambl/prometheus/common/item/base/BaseItem.kt
@@ -3,7 +3,7 @@ package com.williambl.prometheus.common.item.base
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
 
-class BaseItem(registryName: String, creativeTab: CreativeTabs): Item() {
+open class BaseItem(registryName: String, creativeTab: CreativeTabs): Item() {
 
     init {
         this.creativeTab = creativeTab
