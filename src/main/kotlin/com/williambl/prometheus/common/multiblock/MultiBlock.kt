ce9f7b23cc95ffcdf44c26df22077cc3bd10c229
williambl <william@williambl.com>
2019-02-10 00:22:43 +0000

    Improve Multiblock class

--- src/main/kotlin/com/williambl/prometheus/common/multiblock/MultiBlock.kt
+++ src/main/kotlin/com/williambl/prometheus/common/multiblock/MultiBlock.kt
@@ -7,11 +7,26 @@ class MultiBlock(val blocks: Array<MultiBlockBlockInfo>) {
 
     fun getOffsetBlockInfo(originalIndex: Int, offset: BlockPos): MultiBlockBlockInfo {
         val original = blocks[originalIndex]
-        return MultiBlockBlockInfo(original.pos.add(offset), original.blockState)
+        return original.copy(pos = original.pos.add(offset))
     }
 
     fun getBlockInfo(index: Int): MultiBlockBlockInfo {
         return blocks[index]
+    }
+
+    fun getAllOffsetBlockInfos(offset: BlockPos): Array<MultiBlockBlockInfo> {
+        val newBlocks = arrayOfNulls<MultiBlockBlockInfo>(blocks.size)
+        var i = 0
+        blocks.forEach { blockInfo ->
+            newBlocks[i] = blockInfo.copy(pos = blockInfo.pos.add(offset))
+            i++
+        }
+
+        return newBlocks as Array<MultiBlockBlockInfo>
+    }
+
+    fun getAllBlockInfos(): Array<MultiBlockBlockInfo> {
+        return blocks
     }
 
     data class MultiBlockBlockInfo(val pos: BlockPos, val blockState: IBlockState)
