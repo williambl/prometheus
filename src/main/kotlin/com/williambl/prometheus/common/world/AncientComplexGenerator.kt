764b0137084b718f1e9ace14a4c8e55f25fe822c
williambl <william@williambl.com>
2019-07-04 16:16:54 +0100

    Improve Ancient Complex generator

--- src/main/kotlin/com/williambl/prometheus/common/world/AncientComplexGenerator.kt
+++ src/main/kotlin/com/williambl/prometheus/common/world/AncientComplexGenerator.kt
@@ -1,7 +1,6 @@
 package com.williambl.prometheus.common.world
 
 import com.williambl.prometheus.Prometheus
-import net.minecraft.util.Mirror
 import net.minecraft.util.ResourceLocation
 import net.minecraft.util.Rotation
 import net.minecraft.util.math.ChunkPos
@@ -12,20 +11,30 @@ import net.minecraft.world.gen.structure.template.Template
 
 object AncientComplexGenerator {
 
+    val entranceResourceLocation = ResourceLocation(Prometheus.MODID, "entrance")
+    val exitResourceLocation = ResourceLocation(Prometheus.MODID, "exit")
     val roomResourceLocations = listOf(ResourceLocation(Prometheus.MODID, "room1"), ResourceLocation(Prometheus.MODID, "room2"))
     val maxRooms = 5
+    val roomHeight = 32
 
     fun generateComplex(world: World, chunk: ChunkPos) {
+        generateRoom(world, chunk, 0, false, getTemplate(world, entranceResourceLocation))
         for (i in 1..maxRooms) {
-            generateStructure(world, chunk, 30 * i, getTemplate(world, roomResourceLocations.random()))
+            generateRoom(world, chunk, roomHeight * i, i % 2 == 1, getTemplate(world, roomResourceLocations.random()))
         }
+        generateRoom(world, chunk, roomHeight * (maxRooms + 1), false, getTemplate(world, exitResourceLocation))
     }
 
-    private fun generateStructure(world: World, chunk: ChunkPos, y: Int, template: Template) {
-        val position = chunk.getBlock(8, y, 8)
+    private fun generateRoom(world: World, chunk: ChunkPos, y: Int, flip: Boolean, template: Template) {
+        var position = chunk.getBlock(8, y, 8)
 
-        val placementSettings = PlacementSettings().setMirror(Mirror.NONE)
-                .setRotation(Rotation.NONE).setIgnoreEntities(false).setChunk(chunk)
+        if (flip)
+            position = position.add(31, 0, 31)
+        print(position)
+
+        val placementSettings = PlacementSettings()
+                .setRotation(if (flip) Rotation.CLOCKWISE_180 else Rotation.NONE)
+                .setIgnoreEntities(false)
 
         template.addBlocksToWorld(world, position, placementSettings)
     }
