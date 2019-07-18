6b22e668d30fad056dd3450f9861fc2fd98a8593
williambl <william@williambl.com>
2019-07-18 22:03:29 +0100

    Remove some debug lines

--- src/main/kotlin/com/williambl/prometheus/server/command/BreakFourthWallCommand.kt
+++ src/main/kotlin/com/williambl/prometheus/server/command/BreakFourthWallCommand.kt
@@ -6,7 +6,6 @@ import com.williambl.prometheus.common.networking.ShowDialogMessage
 import net.minecraft.command.ICommandSender
 import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.server.MinecraftServer
-import org.lwjgl.input.Mouse
 
 class BreakFourthWallCommand : BaseCommand() {
 
@@ -18,14 +17,6 @@ class BreakFourthWallCommand : BaseCommand() {
     override val permissionLevel = 4
 
     override fun execute(server: MinecraftServer, sender: ICommandSender, args: Array<String>) {
-        Mouse.setGrabbed(false)
         ModPackets.instance.sendTo(ShowDialogMessage("Hello" + FourthWallHelper.username, arrayOf("Yes", "No"), finalAction = 0), sender as EntityPlayerMP)
-        //FourthWallHelper.showFakeMenu()
-        /*FourthWallHelper.showDialog(
-                "Hello, " + FourthWallHelper.username,
-                arrayOf("Yes", "No"),
-                Int.MAX_VALUE
-        ) { FourthWallHelper.showDialog("One button?", arrayOf("No"), Int.MAX_VALUE) { FourthWallHelper.showDialog("Zero?", arrayOf(), 30) { fourthwallhelper.shouldmessuptooltips = true } } }
-        */
     }
 }
